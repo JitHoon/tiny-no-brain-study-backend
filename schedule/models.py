6b4aaeddf2f8680ad542996e787121117cbe6671
JitHoon <101972330+JitHoon@users.noreply.github.com>
2023-04-19 22:22:48 +0900

    [Django] 4. Create Model

--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Schedule(models.Model):
+
+    """Model Definition for Schedule"""
+
+    dplus = models.PositiveIntegerField()  # 스터디 차수
+    place = models.TextField()  # 스터디 장소
+    time = models.DateTimeField(auto_now=True)  # 스터디 시간
+    voted = models.PositiveIntegerField()  # 스터디 투표 인원
+    todo = models.TextField()  # 스터디 내용
