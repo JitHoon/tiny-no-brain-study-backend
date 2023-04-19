f452beb066e3423f073f0bd1d2287653aabf8c64
JitHoon <101972330+JitHoon@users.noreply.github.com>
2023-04-19 23:41:33 +0900

    [Edit] Add __str__() to schedule model

--- schedule/models.py
+++ schedule/models.py
@@ -7,8 +7,11 @@ class Schedule(models.Model):
 
     """Model Definition for Schedule"""
 
-    dplus = models.PositiveIntegerField()  # 스터디 차수
+    dplus = models.TextField()  # 스터디 차수
     place = models.TextField()  # 스터디 장소
     time = models.DateTimeField(auto_now=True)  # 스터디 시간
     voted = models.PositiveIntegerField()  # 스터디 투표 인원
     todo = models.TextField()  # 스터디 내용
+
+    def __str__(self):
+        return self.dplus
