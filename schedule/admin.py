d1143d594c8fd3fdb5d5b54e1defebac738d9e33
JitHoon <101972330+JitHoon@users.noreply.github.com>
2023-04-19 23:39:41 +0900

    [Django] 5. Add Admin Pannel to Model

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Schedule
 
 # Register your models here.
+
+
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    pass
+# @ : decoration : 아래 class가 admin.register(Schedule)를 통제한다.
+# Schedule model에대한 admin.ModelAdmin class를 상속받음
