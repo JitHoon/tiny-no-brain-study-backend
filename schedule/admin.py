c44808d1ebb19f24b647de36c683da195cf342df
JitHoon <101972330+JitHoon@users.noreply.github.com>
2023-04-20 09:59:01 +0900

    [Django] 7. Customize admin pannel

--- schedule/admin.py
+++ schedule/admin.py
@@ -6,6 +6,12 @@ from .models import Schedule
 
 @admin.register(Schedule)
 class ScheduleAdmin(admin.ModelAdmin):
-    pass
+
+    list_display = ("dplus", "time", "voted", "todo")
+    list_filter = ("time", "voted")
+    search_fields = ("voted", "todo")
 # @ : decoration : 아래 class가 admin.register(Schedule)를 통제한다.
 # Schedule model에대한 admin.ModelAdmin class를 상속받음
+
+# 튜플 사용시 ()안에 하나의 element만 들어간다면 ,를 찍어줘야한다.
+# ex. search_fields = ("voted",)
