21547f638c4528a8de132ea632c705b436e67a2b
JitHoon <101972330+JitHoon@users.noreply.github.com>
2023-04-26 23:43:22 +0900

    [Edit] Add some explaination

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+# 일반적인 admin class 대신 User 전용 admin을 상속받아온다.
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
--- users/models.py
+++ users/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+# django에서 제공해주는 user class를 상속받아 pw기능 등 보편적인 기능들을 불러와 활용
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
