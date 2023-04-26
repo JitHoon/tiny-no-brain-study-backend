e7755918efdb303349cdea2db680526dbfbde66c
JitHoon <101972330+JitHoon@users.noreply.github.com>
2023-04-27 00:11:09 +0900

    [Django] 9. Custom User model (overriding)

--- users/models.py
+++ users/models.py
@@ -6,4 +6,17 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    # editable=False 를 통해 사용하지 않음을 명시
+    first_name = models.CharField(max_length=150, editable=False)
+    last_name = models.CharField(max_length=150, editable=False)
+    # 우리의 새로운 데이터를 추가
+    name = models.CharField(max_length=150)
+
+
+# users/models.py
+
+# 아래 주석은 실제 AbstractUser 파일에 있는 기능들이다. 아래 기능들을 사용하지 않도록 overroad 것이다.
+'''
+first_name = models.CharField(_("first name"), max_length=150, blank=True)
+last_name = models.CharField(_("last name"), max_length=150, blank=True)
+'''
